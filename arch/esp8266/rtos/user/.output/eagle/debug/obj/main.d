9c26cf2e517741b8721e203ee2a6ff4a576e6939
zeroflag <zeroflag@debian>
2016-03-20 07:54:19 +0000

    hacking esp

--- arch/esp8266/rtos/user/.output/eagle/debug/obj/main.d
+++ arch/esp8266/rtos/user/.output/eagle/debug/obj/main.d
@@ -1,4 +1,4 @@
 .output/eagle/debug/obj/main.o .output/eagle/debug/obj/main.d : main.S ../../defs.S ../../../../generic/data.S \
  ../../../../generic/macros.S ../../../../generic/outerinterpreter.S \
  ../../innerinterpreter.S ../../primitives.S ../../ext.S \
- ../../../../generic/words.S ../../init.S
+ ../../../../generic/words.S
