b05e04844284488ab82128a480161d589380068c
zeroflag <zeroflag@espbuild>
2016-03-30 15:29:52 -0400

    esp netcon wrapper

--- arch/esp8266/rtos/user/punyforth.h
+++ arch/esp8266/rtos/user/punyforth.h
@@ -1,15 +1,6 @@
 #ifndef __PUNYFORTH_H__
 #define __PUNYFORTH_H__
 
-int forth_div(int a, int b);
-int forth_mod(int a, int b);
-
-void forth_putchar(char c);
-char forth_getchar();
-
 void forth_start();
-int forth_gpio_enable(int num, int dir);
-void forth_gpio_write(int num, int value);
-
  
 #endif
--- arch/esp8266/rtos/user/user_main.c
+++ arch/esp8266/rtos/user/user_main.c
@@ -3,60 +3,10 @@
 #include "espressif/esp_softap.h"
 #include "task.h"
 #include "espressif/esp8266/esp8266.h"
-#include "esp/gpio.h"
 #include "punyforth.h"
-
-#define FORTH_TRUE -1
-#define FORTH_FALSE 0
 
 static void forth_init(void* dummy) {
     forth_start();   
-}
-
-int forth_div(int a, int b) { return a / b; }
-int forth_mod(int a, int b) { return a % b; }
-
-int forth_gpio_enable(int num, int dir) { 
-    gpio_direction_t d;
-    switch (dir) {
-        case 1: d = GPIO_INPUT; break;
-        case 2: d = GPIO_OUTPUT; break;
-        case 3: d = GPIO_OUT_OPEN_DRAIN; break;
-        default: return FORTH_FALSE;
-    }
-    printf("Enabling GPIO %d: %d\n", num, dir);
-    gpio_enable(num, d); 
-    return FORTH_TRUE;
-}
-
-void forth_gpio_write(int num, int value) { 
-    printf("Writing GPIO %d <- %d\n", num, value);
-    gpio_write(num, value == FORTH_TRUE ? true : false); 
-}
-
-void forth_delay(int millis) {
-    vTaskDelay(millis / portTICK_RATE_MS);    
-}
-
-void forth_putchar(char c) { 
-    printf("%c", c);
-}
-
-char forth_getchar() { 
-    return getchar();	
-}
-
-int forth_time() { 
-    return xTaskGetTickCount();
-}
-
-void forth_abort() { 
-    printf("Restarting ESP ..\n");
-    sdk_system_restart();
-}
-
-void forth_type(char* text, int len) { 
-    printf("%.*s", len, text);
 }
 
 void user_init(void) {
