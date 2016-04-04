497f935298e8e4b1a2b52c33f1e6a96b7c1113d3
zeroflag <zeroflag@espbuild>
2016-04-04 08:39:55 -0400

    added spi + random support

--- arch/esp8266/rtos/user/forth_math.c
+++ arch/esp8266/rtos/user/forth_math.c
@@ -1,4 +1,6 @@
+#include "FreeRTOS.h"
+#include "espressif/esp_common.h"
 
 int forth_div(int a, int b) { return a / b; }
 int forth_mod(int a, int b) { return a % b; }
-
+int forth_random() { return rand(); }
