957757b07a588bb3252ef86847bcafebf30f62d7
zeroflag <zeroflag@espbuild>
2016-05-11 00:00:19 -0400

    added buffering to netconn

--- arch/esp8266/rtos/user/forth_netconn.c
+++ arch/esp8266/rtos/user/forth_netconn.c
@@ -4,7 +4,34 @@
 #include "lwip/api.h"
 #include "lwip/ip_addr.h"
 
-struct netconn* forth_netcon_new(int type) {
+#define CACHE_SIZE 256
+
+struct forth_netconn {
+    struct netconn* conn;
+    char *cache;
+    int cache_start;
+    int cache_end;
+};
+
+int cache_data_len(struct forth_netconn* conn) {
+    return conn->cache_end - conn->cache_start; 
+}
+
+int cache_free_space(struct forth_netconn* conn) {
+    return CACHE_SIZE - conn->cache_end; 
+}
+
+struct forth_netconn* make_forth_netconn(struct netconn* conn) {
+    if (conn == NULL) return NULL;
+    struct forth_netconn *result = malloc(sizeof(struct forth_netconn));
+    result->conn = conn;
+    result->cache = (char*) malloc(CACHE_SIZE);
+    result->cache_start = 0;
+    result->cache_end = 0;
+    return result;
+}
+
+struct forth_netconn* forth_netcon_new(int type) {
     enum netconn_type con_type;
     printf("New netcon type: %d\n", type);
     switch (type) {
@@ -14,10 +41,10 @@ struct netconn* forth_netcon_new(int type) {
     }
     struct netconn* conn = netconn_new(con_type);
     printf("Connection ready: %p\n", conn);
-    return conn;
+    return make_forth_netconn(conn);
 }
 
-int forth_netcon_connect(struct netconn* conn, char* host, int port) {   
+int forth_netcon_connect(struct forth_netconn* conn, char* host, int port) {   
     err_t err; ip_addr_t ip;
     printf("Getting hostname: %s\n", host);
     err = netconn_gethostbyname(host, &ip);
@@ -26,18 +53,18 @@ int forth_netcon_connect(struct netconn* conn, char* host, int port) {
         return err;
     }
     printf("Connecting to: %s:%d conn: %p\n", host, port, conn);
-    err = netconn_connect(conn, &ip, (u16_t)(port & 0xFFFF));
+    err = netconn_connect(conn->conn, &ip, (u16_t)(port & 0xFFFF));
     if (err != ERR_OK) {
         printf("Failed to connect to %s:%d. Error: %d\n", host, port, err);
     }
     return err;
 }
 
-int forth_netcon_listen(struct netconn *conn) {
-    return netconn_listen(conn);
+int forth_netcon_listen(struct forth_netconn *conn) {
+    return netconn_listen(conn->conn);
 }
 
-int forth_netcon_bind(struct netconn* conn, char* host, int port) {   
+int forth_netcon_bind(struct forth_netconn* conn, char* host, int port) {   
     err_t err; ip_addr_t ip;
     printf("Getting hostname: %s\n", host);
     err = netconn_gethostbyname(host, &ip);
@@ -46,14 +73,14 @@ int forth_netcon_bind(struct netconn* conn, char* host, int port) {
         return err;
     }
     printf("Binding to: %s:%d conn: %p\n", host, port, conn);
-    err = netconn_bind(conn, &ip, (u16_t)(port & 0xFFFF));
+    err = netconn_bind(conn->conn, &ip, (u16_t)(port & 0xFFFF));
     if (err != ERR_OK) {
         printf("Failed to connect to %s:%d. Error: %d\n", host, port, err);
     }
     return err;
 }
 
-int forth_netcon_send(struct netconn* conn, void* data, int len) {
+int forth_netcon_send(struct forth_netconn* conn, void* data, int len) {
     printf("Sending data len: %d conn: %p\n", len, conn);
     err_t err;
     uint16_t len16 = len;
@@ -66,7 +93,7 @@ int forth_netcon_send(struct netconn* conn, void* data, int len) {
         return ERR_MEM;
     }
     memcpy(memory, data, len16);
-    err = netconn_send(conn, buffer);
+    err = netconn_send(conn->conn, buffer);
     if (err != ERR_OK) {
         printf("Failed to send data. Conn: %p. Error: %d\n", conn, err);
     }   
@@ -74,11 +101,11 @@ int forth_netcon_send(struct netconn* conn, void* data, int len) {
     return err;
 }
 
-int forth_netcon_write(struct netconn* conn, void* data, int len) {
+int forth_netcon_write(struct forth_netconn* conn, void* data, int len) {
     printf("Writing data len: %d conn: %p\n", len, conn);
     err_t err;
     uint16_t len16 = len;
-    err = netconn_write(conn, data, len16, NETCONN_NOCOPY | NETCONN_MORE);
+    err = netconn_write(conn->conn, data, len16, NETCONN_NOCOPY | NETCONN_MORE);
     if (err != ERR_OK) {
         printf("Failed to write data. Conn: %p. Error: %d\n", conn, err);
     }   
@@ -90,11 +117,11 @@ struct recv_res {
     struct netbuf* nbuf;
 };
 
-struct recv_res forth_netcon_recv(struct netconn* conn) {
+struct recv_res forth_netcon_recv(struct forth_netconn* conn) {
     //printf("Receiving from connection: %p\n", conn);
     err_t err;
     struct netbuf *inbuf;
-    err = netconn_recv(conn, &inbuf);
+    err = netconn_recv(conn->conn, &inbuf);
     struct recv_res result = { .code = err, .nbuf = inbuf };
     return result;
 }
@@ -128,48 +155,85 @@ struct recvinto_res {
     int count;
 };
 
-struct recvinto_res forth_netcon_recvinto(struct netconn* conn, void* buffer, int size) {
-    //printf("receiving buffer %p max size: %d\n", buffer, size);
-    err_t err;
-    struct netbuf *inbuf;
-    int offset = 0;
-    while ((err = netconn_recv(conn, &inbuf)) == ERR_OK && size - offset > 0) {
-        offset += netbuf_copy(inbuf, buffer + offset, size - offset);
-        printf("offs: %d\n", offset);
-        netbuf_delete(inbuf);
-    }
-    struct recvinto_res result = { 
-        .code = offset == 0 ? err : ERR_OK, 
-        .count = offset
+struct recvinto_res fillup_full(void *buffer, int size, struct forth_netconn* conn) {
+    memcpy(buffer, conn->cache + conn->cache_start, size);
+    conn->cache_start += size;
+    struct recvinto_res result = {
+        .code = ERR_OK,
+        .count = size
     };
     return result;
+}
+
+struct recvinto_res fillup_partial(void *buffer, int size, struct forth_netconn * conn) {
+    conn->cache_start = 0;
+    conn->cache_end = 0;
+    struct recvinto_res result = {
+        .code = ERR_OK,
+        .count = cache_data_len(conn)
+    };
+    return result;
+}
+
+struct recvinto_res forth_netcon_recvinto(struct forth_netconn* conn, void* buffer, int size) {
+//    printf("receiving buffer %p max size: %d\n", buffer, size);
+    if (size <= cache_data_len(conn)) return fillup_full(buffer, size, conn);
+    if (cache_data_len(conn) > 0) return fillup_partial(buffer, size, conn);
+    conn->cache_start = 0;
+    conn->cache_end = 0;
+    err_t err;
+    struct netbuf *inbuf;
+    while ((err = netconn_recv(conn->conn, &inbuf)) == ERR_OK && cache_free_space(conn) > 0) {
+        conn->cache_end += netbuf_copy(inbuf, conn->cache + conn->cache_end, cache_free_space(conn));
+        netbuf_delete(inbuf);
+    }
+    if (conn->cache_end == 0) {
+        struct recvinto_res result = { 
+            .code = err,
+            .count = 0
+        };
+        return result;
+    }
+    return size <= cache_data_len(conn)
+        ? fillup_full(buffer, size, conn)
+        : fillup_partial(buffer, size, conn);
 }
 
 struct accept_res {
     int code;
-    struct netconn* conn;
+    struct forth_netconn* conn;
 };
 
-struct accept_res forth_netcon_accept(struct netconn* conn) {
+struct accept_res forth_netcon_accept(struct forth_netconn* conn) {
     struct netconn *new_conn;
-    err_t code = netconn_accept(conn, &new_conn);
-    struct accept_res result = {
-        .code =  code,
-        .conn = new_conn
-    };
-    return result;
+    err_t code = netconn_accept(conn->conn, &new_conn);
+    if (code == ERR_OK) {
+        struct accept_res result = {
+            .code = code,
+            .conn = make_forth_netconn(new_conn)
+        };
+        return result;
+    } else {
+        struct accept_res result = {
+            .code = code,
+            .conn = NULL
+        };
+        return result;
+    }
 }
  
-void forth_netcon_close(struct netconn* conn) {
+void forth_netcon_close(struct forth_netconn* conn) {
     printf("Closing connection %p\n", conn);
-    netconn_close(conn);
+    netconn_close(conn->conn);
 }
 
-void forth_netcon_delete(struct netconn* conn) {
+void forth_netcon_delete(struct forth_netconn* conn) {
     printf("Deleting connection %p\n", conn);
-    netconn_delete(conn);
+    netconn_delete(conn->conn);
+    free(conn->cache);
+    free(conn);
 }
 
-void forth_netcon_set_recvtimeout(struct netconn* conn, int timeout) {
-    netconn_set_recvtimeout(conn, timeout);
+void forth_netcon_set_recvtimeout(struct forth_netconn* conn, int timeout) {
+    netconn_set_recvtimeout(conn->conn, timeout);
 }
