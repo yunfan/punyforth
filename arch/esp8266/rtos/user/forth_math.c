3f4f9a7fe83790dc4c16c0e05ef8944bc3edf669
zeroflag <zeroflag@espbuild>
2016-05-11 23:40:44 -0400

    divmod optimalization

--- arch/esp8266/rtos/user/forth_math.c
+++ arch/esp8266/rtos/user/forth_math.c
@@ -1,6 +1,7 @@
 #include "FreeRTOS.h"
 #include "espressif/esp_common.h"
 
+div_t forth_divmod(int a, int b) { return div(a,b); }
 int forth_div(int a, int b) { return a / b; }
 int forth_mod(int a, int b) { return a % b; }
 int forth_random() { return rand(); }
