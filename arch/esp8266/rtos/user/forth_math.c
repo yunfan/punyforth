6bcb88f92858eba91318ca72bf82e899aef955d5
zeroflag <zeroflag@espbuild>
2016-05-14 00:06:16 -0400

    removed unused

--- arch/esp8266/rtos/user/forth_math.c
+++ arch/esp8266/rtos/user/forth_math.c
@@ -2,6 +2,4 @@
 #include "espressif/esp_common.h"
 
 div_t IRAM forth_divmod(int a, int b) { return div(a,b); }
-int IRAM forth_div(int a, int b) { return a / b; }
-int IRAM forth_mod(int a, int b) { return a % b; }
 int IRAM forth_random() { return rand(); }
